139fb902b64d045383274658971f4420e20c6ca8
NN-Bayat <noorullahbayat1414@gmail.com>
2025-03-13 13:02:52 +0430

    feat(setup): besic files installed

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <main>
+      <h1 className='text-5xl font-bold'>something to know it's works correctly</h1>
+    </main>
+  )
+}
+
+export default App
